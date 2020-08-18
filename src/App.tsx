95d8e3f0ac5a1849354dc0b725ef9954cc4fb502
易东择 <bi1ebi@hotmail.com>
2020-08-18 19:15:18 +0800

    主页面

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Dad from "./comps/dad_and_bros";
+import DadB from "./comps/dad_and_bros_B";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Passing values with Hooks in TS</h1>
+			<hr/>
+			<Dad/>
+			<hr/>
+			<DadB/>
+		</div>
+	);
 }
 
 export default App;
